667ad6590461d6a29f9b149e83cd14d8b4f560f2
Joshua Prila <jprila@hawk.iit.edu>
2023-03-11 13:39:39 -0600

    Set to LDPP 2018

--- test2.py
+++ test2.py
@@ -7,7 +7,7 @@ df = pd.read_csv('ninetyData.csv', index_col = 'LDPC_2021')
 #print(df.describe)
 
 scaler = StandardScaler
-print(df.describe())
+print(df.index.names)
 
 #df['LDPC_2021_T', 'LDPC_2020_T', 'LDPC_2019_T', 'LDPC_2018_T', 'LDPC_2017_T'] = scaler.fit_transform(df['LDPC_2021', 'LDPC_2020', 'LDPC_2019', 'LDPC_2018', 'LDPC_2017'])
 #print(df[LDPC_2021_T, LDPC_2020_T, LDPC_2019_T, LDPC_2018_T, LDPC_2017_T])
